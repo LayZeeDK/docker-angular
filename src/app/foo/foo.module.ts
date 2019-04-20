338e48a5dacecf1ed81b31debff8e190916ca2cc
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2019-04-20 22:28:08 +0200

    add routed module

---
+++ src/app/foo/foo.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { FooRoutingModule } from './foo-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    FooRoutingModule
+  ]
+})
+export class FooModule { }
