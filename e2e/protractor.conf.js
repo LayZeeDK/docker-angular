500f1187fa650891b9e275c97636df8cd07566dc
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2019-04-22 22:51:14 +0200

    add test Dockerfile

--- e2e/protractor.conf.js
+++ e2e/protractor.conf.js
@@ -2,6 +2,7 @@
 // https://github.com/angular/protractor/blob/master/lib/config.ts
 
 const { SpecReporter } = require('jasmine-spec-reporter');
+// const puppeteer = require('puppeteer');
 
 exports.config = {
   allScriptsTimeout: 11000,
@@ -10,11 +11,20 @@ exports.config = {
   ],
   capabilities: {
     browserName: 'chrome',
+    // chromeDriver: '/usr/bin/chromium-browser',
     chromeOptions: {
-      args: ['--no-sandbox', '--headless', '--disable-gpu', '--remote-debugging-port=9222']
+      args: ['--no-sandbox', '--headless', '--disable-gpu', '--remote-debugging-port=9222', '--window-size=1024,768'],
+      // binary: puppeteer.executablePath(),
+      // chromeDriver: require('path').join(__dirname, '../node_modules/webdriver-manager/selenium/chromedriver_2.46'),
     }
   },
+  // chromeDriver: '/usr/bin/chromium-browser',
+  // chromeDriver: require('path').join(__dirname, '../node_modules/webdriver-manager/selenium/chromedriver_2.46'),
+  // ignoreUncaughtExceptions: true,
   directConnect: true,
+  // directConnect: false,
+  // seleniumAddress: 'http://localhost:4444/wd/hub',
+  // seleniumAddress: 'http://selenium-chrome:4444/wd/hub',
   baseUrl: 'http://localhost:4200/',
   framework: 'jasmine',
   jasmineNodeOpts: {
